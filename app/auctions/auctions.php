a4c9911e546bc4ecee2f9cf770b14049a61ebf87
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2022-12-22 23:29:39 +0100

    ✨ frontend for auctions

---
+++ app/auctions/auctions.php
@@ -0,0 +1,73 @@
+<div class="main_content">
+	<div class="main_content_header">
+		<div class="main_content_header_icon">
+			<i class="fi fi-sr-bank"></i>
+		</div>
+		<div class="main_content_header_text">
+			Auctions
+		</div>
+	</div>
+	<div class="df fdc fg-10 main_content_context">
+		<div>
+			<div class="content_header df jcsb">
+				<span>Aktive auksjoner</span>
+			</div>
+			<div class="content_context">
+				Under vil du se aktive auksjoner. Dersom auksjonen er ferdig vil
+				det være en indikasjon på at auksjonen er avsluttet.
+			</div>
+
+
+			<!-- kuleauksjon sample start -->
+			<div class="mt-5 content_context df aic jcsb">
+				<div class="df fg-10 aic">
+					<div>
+						<iconify-icon class="medium_icon" icon="mdi:bullets"></iconify-icon>
+					</div>
+					<span>Type: <b>Kuler</b></span>
+				</div>
+				<span>Antall: <b>20 stk.</b></span>
+				<span>Selger: <a class="secondary-link" href="#">Dope</a></span>
+				<span class="auction-status-active">Aktiv nå</span>
+				<span>Bud: <b>20 000 000 kr</b></span>
+				<a class="primary-link" href="#">Legg inn bud</a>
+			</div>
+			<!-- kuleauksjon sample end -->
+
+
+			<!-- poengauksjon sample start -->
+			<div class="mt-5 content_context df aic jcsb">
+				<div class="df fg-10 aic">
+					<div>
+					<iconify-icon class="medium_icon" icon="fa:diamond"></iconify-icon>
+					</div>
+					<span>Type: <b>Poeng</b></span>
+				</div>
+				<span>Antall: <b>50 stk.</b></span>
+				<span>Selger: <a class="secondary-link" href="#">Skitzo</a></span>
+				<span class="auction-status-active">Aktiv nå</span>
+				<span>Bud: <b>35 000 000 kr</b></span>
+				<a class="primary-link" href="#">Legg inn bud</a>
+			</div>
+			<!-- poengauksjon sample end -->
+
+
+
+			<!-- firma sample start -->
+			<div class="mt-5 content_context df aic jcsb">
+				<div class="df fg-10 aic">
+					<div>
+					<iconify-icon class="medium_icon" icon="ic:sharp-factory"></iconify-icon>
+					</div>
+					<span>Type: <b>Bilverksted</b></span>
+				</div>
+				<span>By: <b>Bawserville</b></span>
+				<span>Selger: <a class="secondary-link" href="#">Skitzo</a></span>
+				<span class="auction-status-end">Avsluttet 25:55</span>
+				<span>Vinner: <a class="secondary-link" href="#">Bawser</a> (255 000 000 kr)</span>
+			</div>
+			<!-- firma sample end -->
+
+		</div>
+	</div>
+</div>
