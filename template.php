9bc9d5f0842b81cbc33dc2bc0d8b2a6510ee848b
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2022-12-22 23:03:20 +0100

    ✨ improve template file

--- template.php
+++ template.php
@@ -1,18 +1,20 @@
-<div class="main_content_header">
-	<div class="main_content_header_icon">
-		<i class="fi fi-sr-bank"></i>
-	</div>
-	<div class="main_content_header_text">
-		Some header
-	</div>
-</div>
-<div class="df fdc fg-10 main_content_context">
-	<div>
-		<div class="content_header df jcsb">
-			<span>Some subheader</span>
+<div class="main_content">
+	<div class="main_content_header">
+		<div class="main_content_header_icon">
+			<i class="fi fi-sr-bank"></i>
 		</div>
-		<div class="content_context">
-			Some context
+		<div class="main_content_header_text">
+			Some header
+		</div>
+	</div>
+	<div class="df fdc fg-10 main_content_context">
+		<div>
+			<div class="content_header df jcsb">
+				<span>Some subheader</span>
+			</div>
+			<div class="content_context">
+				Some context
+			</div>
 		</div>
 	</div>
 </div>
