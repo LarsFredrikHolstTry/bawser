2b2aea49bcabe8465133b99f845fb3e4e76075a1
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2022-12-30 12:58:04 +0100

    ✨ backend for register user

--- homepage/components/_register.php
+++ homepage/components/_register.php
@@ -10,17 +10,42 @@ $password = $_POST['password'];
 $repeatPassword = $_POST['repeatPassword'];
 
 $usernameExist = $db->run("SELECT ACC_name FROM account WHERE ACC_name = ?", [$username])->fetchAll();
+$emailExist = $db->run("SELECT ACC_mail FROM account WHERE ACC_mail = ?", [$email])->fetchAll();
 
 if($password != $repeatPassword){
-	echo 'Ikke lik passord ' . '<|>' . 'error';
+	echo 'Passordene stemmer ikke overens med hverandre ' . '<|>' . 'error';
 } elseif(strlen($username) < 3 || strlen($username) > 20){
 	echo 'Brukernavnet må være mellom 3 og 20 tegn ' . '<|>' . 'error';
 } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 	echo 'Emailen er ikke gyldig ' . '<|>' . 'error';
+} elseif($emailExist){
+	echo 'E-posten eksisterer fra før ' . '<|>' . 'error';
 } elseif($usernameExist){
 	echo 'Brukernavnet eksisterer fra før ' . '<|>' . 'error';
+} elseif(preg_match("/^[a-zA-Z0-9]+$/", $username) != 1) {
+	echo 'Brukernavnet kan kun inneholde a-z, A-Z og 0-9 ' . '<|>' . 'error';
 } else {
 	$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
+	$values = [
+		'ACC_name' => $username,
+		'ACC_password' => $hashed_password,
+		'ACC_mail' => $email,
+		'ACC_register' => time(),
+		'ACC_last_active' => time(),
+	];
+	$db->insert('account', $values);
+	$ACC_id = $db->lastInsertId();
+
+	$values = [
+		'VERI_uniqid' => uniqid(),
+		'VERI_acc_id' => $ACC_id,
+		'VERI_date' => time(),
+	];
+	$db->insert('verification', $values);
+
+	$_SESSION['ID'] = $ACC_id;
+
 	echo 'Du er registrert! ' . '<|>' . 'success';
-}
+}	
+
