3a9586c3f26bac4829d813d01d2d3f23dd17a83d
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2022-12-31 19:09:20 +0100

    🎨 get data from backend

--- homepage/components/left_menu.php
+++ homepage/components/left_menu.php
@@ -1,52 +1,42 @@
+	<div class="left_menu">
+		<div class="aic df jcsb shadow left_menu_element">
+			<span>Topplisten siste timen</span>
+		</div>
+	<?php
+	
+	/**
+	 * TODO: Order by EXP last hour
+	 * COMMENT: Currently no order because there is no table with EXP
+	 */
+	$allPlayers = $db->run("SELECT * FROM account")->fetchAll();
+	foreach($allPlayers as $players){
 
-<div class="left_menu">
-	<div class="aic df jcsb shadow left_menu_element">
-		<span>Topplisten siste timen</span>
-	</div>
-	<div class="left_menu_element shadow df aic fg-5">
-		<span class="menu_cooldown">1</span>
-		<span>Fredrico <span class="text-secondary">• Gateselger</span></span>
-	</div>
-	<div class="left_menu_element shadow df aic fg-5">
-		<span class="menu_cooldown">2</span>
-		<span>Dope <span class="text-secondary">• Ingeniør</span</span>
-	</div>
-	<div class="left_menu_element shadow df aic fg-5">
-		<span class="menu_cooldown">3</span>
-		<span>WillyWonka <span class="text-secondary">• Gudfar</span</span></span>
-	</div>
-	<div class="left_menu_element shadow df aic fg-5">
-		<span class="menu_cooldown">4</span>
-		<span>Piccolo <span class="text-secondary">• Drosjesjåfør</span</span>
-	</div>
-	<div class="left_menu_element shadow df aic fg-5">
-		<span class="menu_cooldown">5</span>
-		<span>Killzone <span class="text-secondary">• Gudfar</span></span>
-	</div>
+	?>
+		<div class="left_menu_element shadow df aic fg-5">
+			<span class="menu_cooldown">1</span>
+			<span><?= $players['ACC_name'] ?><span class="text-secondary"> • Null</span></span>
+		</div>
+	<?php } ?>
+
 	<div class="aic df jcsb shadow no_link">
 		<span></span>
 	</div>
 	<div class="aic df jcsb shadow left_menu_element">
 		<span>Leaderboard</span>
 	</div>
-	<div class="left_menu_element shadow df aic fg-5">
-		<span class="menu_cooldown">1</span>
-		<span>Skitzo <span class="text-secondary">• Gudfar</span></span>
-	</div>
-	<div class="left_menu_element shadow df aic fg-5">
-		<span class="menu_cooldown">2</span>
-		<span>Cobraz <span class="text-secondary">• Gudfar</span></span>
-	</div>
-	<div class="left_menu_element shadow df aic fg-5">
-		<span class="menu_cooldown">3</span>
-		<span>WillyWonka <span class="text-secondary">• Gudfar</span></span>
-	</div>
-	<div class="left_menu_element shadow df aic fg-5">
-		<span class="menu_cooldown">4</span>
-		<span>Xooomy <span class="text-secondary">• Gudfar</span></span>
-	</div>
-	<div class="left_menu_element shadow df aic fg-5">
-		<span class="menu_cooldown">5</span>
-		<span>Killzone <span class="text-secondary"><span class="text-secondary">• Gudfar</span></span></span>
-	</div>
+	<?php
+
+	/**
+	 * TODO: Order by total EXP
+	 * COMMENT: Currently no order because there is no table with EXP
+	 */
+	$allPlayers = $db->run("SELECT * FROM account")->fetchAll();
+	foreach($allPlayers as $players){
+
+	?>
+		<div class="left_menu_element shadow df aic fg-5">
+			<span class="menu_cooldown">1</span>
+			<span><?= $players['ACC_name'] ?><span class="text-secondary"> • Null</span></span>
+		</div>
+	<?php } ?>
 </div>
