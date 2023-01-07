d1d1a7bfce07307753f21fa7d4099ca1c47491e8
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2023-01-07 22:57:55 +0100

    ✨ add frontend for gang

---
+++ app/family/family.php
@@ -0,0 +1,155 @@
+<div class="main_content">
+	<div class="main_content_header">
+		<div class="main_content_header_icon">
+			<i class="fi fi-sr-bank"></i>
+		</div>
+		<div class="main_content_header_text">
+			Gjengsenter
+		</div>
+	</div>
+	<div class="df fdc fg-10 main_content_context">
+		<div class="innerDiv">
+			<div class="content_header df jcsb">
+				<span>
+					Din gjeng: <a class="primary-link" href="#">The Skitzos</a> • 
+					Status: <a class="primary-link" href="#">Leder</a> • 
+					Medlemmer: <a class="primary-link" href="#">8 stk</a> • 
+					Gjengranking: <a class="primary-link" href="#">#2</a></span>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="df">
+		<div style="flex-basis: 50%;"class="df fdc fg-10 main_content_context">
+			<div class="innerDiv">
+				<a href="#">
+					<div class="bb-1 df jcsb mt-0 content_context_ready content_context content_context_narrow">
+						<span>Organisert gjengaktivitet</span>
+						<span>Klar</span>
+					</div>
+				</a>
+				<a href="#">
+					<div class="bb-1 df jcsb mt-0 content_context content_context_narrow">
+						<span>Firmautpressing</span>
+						<span>1 dag, 54 min, 24 sek</span>
+					</div>
+				</a>
+				<a href="#">
+					<div class="bb-1 df jcsb mt-0 content_context content_context_narrow">
+						<span>Gjengbank</span>
+						<span>100 000 000 kr</span>
+					</div>
+				</a>
+				<a href="#">
+					<div class="bb-1 df jcsb mt-0 content_context content_context_narrow">
+						<span>Kulelager</span>
+						<span>100 kuler</span>
+					</div>
+				</a>
+				<a href="#">
+					<div class="bb-1 df jcsb mt-0 content_context content_context_narrow">
+						<span>Forsvar</span>
+					</div>
+				</a>
+				<a href="#">
+					<div class="bb-1 df jcsb mt-0 content_context content_context_narrow">
+						<span>Territorium</span>
+					</div>
+				</a>
+				<a href="#">
+					<div class="df jcsb mt-0 content_context content_context_narrow">
+						<span>Virksomhet</span>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div style="flex-basis: 50%;" class="df fdc fg-10 main_content_context">
+			<div class="innerDiv">
+			<div class="content_header">
+					CobrazArne • Oversikt
+				</div>
+				<div class="content_context">
+					<ul>
+						<li class="big_padding">Leder: <a class="primary-link" href="#">Cobraz</a></li>
+						<li class="big_padding">Medlemmer: <a class="primary-link" href="#">2 stk</a></li>
+						<li class="big_padding">Gjengranking: <a class="primary-link" href="#">#2</a></li>
+						<li class="big_padding">Penger tjent idag: 10 000 000 kr</li>
+						<li class="big_padding">Penger tjent siste uken: 120 000 000 kr</li>
+						<li class="big_padding">EXP ranket idag: 297</li>
+						<li class="big_padding">EXP ranket denne uken: 2567</li>
+						<li class="big_padding">Familiebank: 100 000 000 kr • <a class="success-link" href="#">Donasjon</a></li>
+						<li class="big_padding">Firmaer: 3 stk</li>
+						<li class="big_padding">Gjeng opprettet: 01.01.2023</li>
+					</ul>
+					<div class="mt-10 df fg-10">
+						<a class="primary-link" href="#">Innstillinger</a> • <a class="primary-link" href="#">Søknader</a> • <a class="primary-link" href="#">Gjengforum</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<div class="df fdc fg-10 main_content_context">
+			<div class="innerDiv">
+				<div class="content_header tac">
+					<span>Firmaer</span>
+				</div>
+				<!-- <table class="table">
+					<tr class="table-header">
+						<th>Navn</th>
+						<th>Type</th>
+						<th>Eies av</th>
+						<th>Ukentlig avkastning</th>
+						<th>Total avkastning til gjeng</th>
+					</tr>
+					<tr>
+						<td>Lorem</td>
+						<td>Lorem</td>
+						<td>Lorem</td>
+						<td>Lorem</td>
+						<td>Lorem</td>
+					</tr>
+				</table> -->
+				<div class="content_context">
+					<?= alert('default', 'Gjengen har ingen firmaer') ?>
+				</div>
+			</div>
+		</div>
+
+		<div class="df fdc fg-10 main_content_context">
+			<div class="innerDiv">
+				<div class="content_header tac">
+					<span>Medlemsoversikt (2 stk)</span>
+				</div>
+				<table class="table">
+					<tr class="table-header">
+						<th>Navn</th>
+						<th>Familiestatus</th>
+						<th>EXP i dag</th>
+						<th>EXP totalt</th>
+						<th>Bidrag til familien</th>
+						<th>Medlem siden</th>
+					</tr>
+					<tr>
+						<td>Cobraz</td>
+						<td>Leder</td>
+						<td>23</td>
+						<td>23 034</td>
+						<td>1 002 333 222 kr</td>
+						<td>01.01.2023</td>
+					</tr>
+					<tr>
+						<td>Skitzo</td>
+						<td>Medlem</td>
+						<td>13</td>
+						<td>53 034</td>
+						<td>302 333 222 kr</td>
+						<td>23.02.2023</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+</div>
