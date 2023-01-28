d37625c291df14a057980f83fd4fb872704aab08
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2023-01-28 09:27:18 +0100

    ✨ Ability to create gangs

--- app/gang/gang.php
+++ app/gang/gang.php
@@ -1,7 +1,18 @@
+<?php
+
+$price = 10000000;
+
+$gang_member = $db->run("SELECT * FROM gang_members WHERE GAME_acc_id = ?", [$_SESSION['ID']])->fetch();
+$gangs = $db->run("SELECT * FROM gang")->fetchAll();
+
+if($gang_member){
+	include 'gang_dashboard.php';
+} else {
+?>
 <div class="main_content">
 	<div class="main_content_header">
 		<div class="main_content_header_icon">
-			<i class="fi fi-sr-bank"></i>
+			<iconify-icon class="iconify-for-header" icon="mdi:hat-fedora"></iconify-icon>
 		</div>
 		<div class="main_content_header_text">
 			Gjengsenter
@@ -9,147 +20,124 @@
 	</div>
 	<div class="df fdc fg-10 main_content_context">
 		<div class="innerDiv">
-			<div class="content_header df jcsb">
+			<div class="content_context_narrow-2 df jcsb">
 				<span>
-					Din gjeng: <a class="primary-link" href="#">The Skitzos</a> • 
-					Status: <a class="primary-link" href="#">Leder</a> • 
-					Medlemmer: <a class="primary-link" href="#">8 stk</a> • 
-					Gjengranking: <a class="primary-link" href="#">#2</a></span>
+					Du er ikke medlem i en gjeng. For å bli medlem må du <a class="secondary-link" href="#">sende en søknad</a>
 			</div>
 		</div>
 	</div>
-
-
-	<div class="df">
-		<div style="flex-basis: 50%;"class="df fdc fg-10 main_content_context">
-			<div class="innerDiv">
-				<a href="#">
-					<div class="bb-1 df jcsb mt-0 content_context_ready content_context content_context_narrow">
-						<span>Organisert gjengaktivitet</span>
-						<span>Klar</span>
-					</div>
-				</a>
-				<a href="#">
-					<div class="bb-1 df jcsb mt-0 content_context content_context_narrow">
-						<span>Firmautpressing</span>
-						<span>1 dag, 54 min, 24 sek</span>
-					</div>
-				</a>
-				<a href="#">
-					<div class="bb-1 df jcsb mt-0 content_context content_context_narrow">
-						<span>Gjengbank</span>
-						<span>100 000 000 kr</span>
-					</div>
-				</a>
-				<a href="#">
-					<div class="bb-1 df jcsb mt-0 content_context content_context_narrow">
-						<span>Kulelager</span>
-						<span>100 kuler</span>
-					</div>
-				</a>
-				<a href="#">
-					<div class="bb-1 df jcsb mt-0 content_context content_context_narrow">
-						<span>Forsvar</span>
-					</div>
-				</a>
-				<a href="#">
-					<div class="bb-1 df jcsb mt-0 content_context content_context_narrow">
-						<span>Territorium</span>
-					</div>
-				</a>
-				<a href="#">
-					<div class="df jcsb mt-0 content_context content_context_narrow">
-						<span>Virksomhet</span>
-					</div>
-				</a>
-			</div>
-		</div>
-		<div style="flex-basis: 50%;" class="df fdc fg-10 main_content_context">
-			<div class="innerDiv">
-			<div class="content_header">
-					CobrazArne • Oversikt
-				</div>
-				<div class="content_context">
-					<ul>
-						<li class="big_padding">Leder: <a class="primary-link" href="#">Cobraz</a></li>
-						<li class="big_padding">Medlemmer: <a class="primary-link" href="#">2 stk</a></li>
-						<li class="big_padding">Gjengranking: <a class="primary-link" href="#">#2</a></li>
-						<li class="big_padding">Penger tjent idag: 10 000 000 kr</li>
-						<li class="big_padding">Penger tjent siste uken: 120 000 000 kr</li>
-						<li class="big_padding">EXP ranket idag: 297</li>
-						<li class="big_padding">EXP ranket denne uken: 2567</li>
-						<li class="big_padding">Familiebank: 100 000 000 kr • <a class="success-link" href="#">Donasjon</a></li>
-						<li class="big_padding">Firmaer: 3 stk</li>
-						<li class="big_padding">Gjeng opprettet: 01.01.2023</li>
-					</ul>
-					<div class="mt-10 df fg-10">
-						<a class="primary-link" href="#">Innstillinger</a> • <a class="primary-link" href="#">Søknader</a> • <a class="primary-link" href="#">Gjengforum</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-
 
 	<div class="df fdc fg-10 main_content_context">
-			<div class="innerDiv">
-				<div class="content_header tac">
-					<span>Firmaer</span>
-				</div>
-				<!-- <table class="table">
-					<tr class="table-header">
-						<th>Navn</th>
-						<th>Type</th>
-						<th>Eies av</th>
-						<th>Ukentlig avkastning</th>
-						<th>Total avkastning til gjeng</th>
-					</tr>
-					<tr>
-						<td>Lorem</td>
-						<td>Lorem</td>
-						<td>Lorem</td>
-						<td>Lorem</td>
-						<td>Lorem</td>
-					</tr>
-				</table> -->
-				<div class="content_context content_context_narrow">
-					<?= alert('default', 'Gjengen har ingen firmaer') ?>
-				</div>
+		<div class="innerDiv">
+			<div class="tac content_header">
+				<span>Gjengoversikt</span>
 			</div>
-		</div>
+			<div class="content_context_narrow-2">
+				<?php
 
-		<div class="df fdc fg-10 main_content_context">
-			<div class="innerDiv">
-				<div class="content_header tac">
-					<span>Medlemsoversikt (2 stk)</span>
-				</div>
-				<table class="table">
-					<tr class="table-header">
-						<th>Navn</th>
-						<th>Familiestatus</th>
-						<th>EXP i dag</th>
-						<th>EXP totalt</th>
-						<th>Bidrag til familien</th>
-						<th>Medlem siden</th>
-					</tr>
-					<tr>
-						<td>Cobraz</td>
-						<td>Leder</td>
-						<td>23</td>
-						<td>23 034</td>
-						<td>1 002 333 222 kr</td>
-						<td>01.01.2023</td>
-					</tr>
-					<tr>
-						<td>Skitzo</td>
-						<td>Medlem</td>
-						<td>13</td>
-						<td>53 034</td>
-						<td>302 333 222 kr</td>
-						<td>23.02.2023</td>
-					</tr>
-				</table>
+					if(!$gangs){
+						echo alert('default', 'Det finnes ingen familier');
+					} else {
+						?>
+						<table class="table">
+							<tr>
+								<th style="width: 1%;"></th>
+								<th>Navn</th>
+								<th>Leder</th>
+								<th>Firmaer</th>
+								<th>By</th>
+								<th>Medlemmer</th>
+								<th>Opprettet</th>
+							</tr>
+							<?php
+
+							$i = 0;
+
+							foreach($gangs as $gang){
+								$leader = $db->run("SELECT GAME_acc_id FROM gang_members WHERE GAME_gang = ? AND GAME_status = 1", [$gang['GANG_id']])->fetchColumn();
+								$members = $db->run("SELECT count(*) FROM gang_members WHERE GAME_gang = ?", [$gang['GANG_id']])->fetchColumn();
+
+								$i++;
+
+								?>
+								<tr>
+									<td>#<?= $i ?></td>
+									<td><?= $gang['GANG_name'] ?></td>
+									<td><?= $leader ?></td>
+									<td>null</td>
+									<td><?= $city[$gang['GANG_city']] ?></td>
+									<td><?= $members ?></td>
+									<td><?= date_to_text_long($gang['GANG_created']); ?></td>
+								</tr>
+								<?php
+
+							}
+
+							?>
+						</table>
+						<?php
+
+					}
+
+				?>
 			</div>
 		</div>
+	</div>
+
+	<div class="df fdc fg-10 main_content_context">
+		<div class="innerDiv">
+			<div class="tac content_header">
+				<span>Opprett gjeng</span>
+			</div>
+			<form method="post">
+				<div class="content_context_narrow-2 df fdc fg-10 tac">
+				<?php 
+
+					if(isset($_POST['make_gang'])){
+						$gang_name = $_POST['gang_name'];
+						$gang_exist = $db->run("SELECT * FROM gang WHERE GANG_name = ?", [$gang_name])->fetch();
+
+						if($price > $user_values['UV_money']){
+							echo alert('error', 'Du har ikke nok penger til å opprette en gjeng');
+						} elseif($gang_exist){
+							echo alert('error', 'Familienavnet er ikke ledig');
+						} else {
+
+							$gang = [
+								'GANG_name' => $gang_name,
+								'GANG_city' => $user_values['UV_city'],
+								'GANG_created' => time()
+							];
+							$db->insert('gang', $gang);
+							$gang_id = $db->lastInsertId();
+
+							$gang_member = [
+								'GAME_acc_id' => $_SESSION['ID'],
+								'GAME_gang' => $gang_id,
+								'GAME_status' => 1,
+								'GAME_joined' => time()
+							];
+							$db->insert('gang_members', $gang_member);
+							
+							header("Location: ?page=gang");
+						}
+					}
+
+					?>
+					<span>Det er 6 av 6 ledige gjengplasser</span>
+					<span>Det koster <?= number($price) ?> kr å opprette ny gjeng</span>
+					<div>
+						<input type="text" name="gang_name" placeholder="Skriv inn gjengnavn" />
+					</div>
+					<div>
+						<input type="submit" name="make_gang" class="btn success_btn" value="Opprett gjeng" />
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
 </div>
+
+<?php
+}
+?>
