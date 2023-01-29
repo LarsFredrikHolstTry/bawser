d7a4641728c54e2d2270b98cb6c3dd86d5884ac0
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2023-01-29 17:33:30 +0100

    💄 Add one more alternative for GTA to test hover

--- app/gta/_gta.php
+++ app/gta/_gta.php
@@ -17,3 +17,12 @@ $gta_money_from[1] = 1;
 $gta_money_to[1] = 50000;
 $gta_exp[1] = 2;
 $gta_cooldown[1] = 120;
+
+$gta[2] = 'Stjel en bil fra luksus bilforhandler';
+$gta_chance[2] = 55;
+$gta_success[2] = 23;
+$gta_fails[2] = 35;
+$gta_money_from[2] = 1;
+$gta_money_to[2] = 50000;
+$gta_exp[2] = 2;
+$gta_cooldown[2] = 120;
