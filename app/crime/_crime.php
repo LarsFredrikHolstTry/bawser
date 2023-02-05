788e188a97e5f5024f63b5a244d79ba4ea281aa8
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2023-02-05 09:43:17 +0100

    ✨ Crime frontend

--- app/crime/_crime.php
+++ app/crime/_crime.php
@@ -6,4 +6,32 @@ if($submittedID){
 	echo alert('warning', 'Du utførte kriminalitet og fikk 10 exp og 50 kr');
 }
 
+$crime[0] = 'Lorem Ipsum Kriminalitet';
+$crime_chance[0] = 55;
+$crime_success[0] = 15;
+$crime_fails[0] = 25;
+$crime_money_from[0] = 1;
+$crime_money_to[0] = 50000;
+$crime_exp[0] = 2;
+$crime_cooldown[0] = 120;
+
+$crime[1] = 'Lorem Ipsum Kriminalitet';
+$crime_chance[1] = 55;
+$crime_success[1] = 23;
+$crime_fails[1] = 35;
+$crime_money_from[1] = 1;
+$crime_money_to[1] = 50000;
+$crime_exp[1] = 2;
+$crime_cooldown[1] = 120;
+
+$crime[2] = 'Lorem Ipsum Kriminalitet';
+$crime_chance[2] = 55;
+$crime_success[2] = 23;
+$crime_fails[2] = 35;
+$crime_money_from[2] = 1;
+$crime_money_to[2] = 50000;
+$crime_exp[2] = 2;
+$crime_cooldown[2] = 120;
+
+
 ?>
--- app/crime/crime.php
+++ app/crime/crime.php
@@ -1,64 +1,88 @@
+<?php
+
+include '_crime.php';
+
+?>
+
 <div class="main_content">
 	<div class="main_content_header">
 		<div class="main_content_header_icon">
-			<i class="fi fi-sr-bank"></i>
+		<iconify-icon class="iconify-for-header" icon="mdi:gun"></iconify-icon>
 		</div>
 		<div class="main_content_header_text">
-			Crime
+			Kriminalitet
 		</div>
 	</div>
+	
 	<div class="df fdc fg-10 main_content_context">
-		<div class="innerDiv">
-			<?php
-
-			include '_crime.php';
-
-			?>
-			<div class="content_header df jcsb">
-				<span>Crime</span>
-			</div>
-			<div class="content_context">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim, 
+		<div class="tac py-1 text-dark">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim, 
 			eros at suscipit bibendum, elit lacus venenatis magna, quis molestie 
 			lectus eros sit amet orci. Nam aliquet nibh quis ligula suscipit, ac 
 			viverra tortor gravida. Nam ac sollicitudin augue. Suspendisse tempor 
 			velit sit amet nulla placerat, nec congue tortor aliquam. Nulla a accumsan 
 			velit. In sapien tellus, volutpat et neque ac, viverra congue orci. 
 			Cras neque lacus, maximus nec gravida sed, faucibus sed ante.
-			<form method="post" id="crimeForm" action="">
-				<input name="cat" type="hidden"/>
-				<table class="table mt-5">
-					<tr class="table-header">
-						<th>Handling</th>
-						<th>Ventetid</th>
-						<th>Sjanse</th>
-						<th>Utbetaling</th>
-					</tr>
-					<tr value="hei" class="table-row-clickable">
-						<td>Rob Lorem Ipsum</td>
-						<td>15s</td>
-						<td>80%</td>
-						<td>10 kr - 50 kr</td>
-					</tr>
-					<tr class="table-row-clickable">
-						<td>Rob Lorem Ipsum</td>
-						<td>25s</td>
-						<td>70%</td>
-						<td>50 kr - 150 kr</td>
-					</tr>
-					<tr class="table-row-clickable">
-						<td>Rob Lorem Ipsum</td>
-						<td>45s</td>
-						<td>60%</td>
-						<td>110 kr - 250 kr</td>
-					</tr>
-				</table>
-				</form>
+		</div>
+		<div id="next-cards" class="df fdc fg-5">
+			<?php for($i = 0; $i < count($crime); $i++){ 
+				$percentage_fail_success = $crime_success[$i] / $crime_fails[$i] * 100;
+				$percentage_fail_success_str = $percentage_fail_success > 100 ? 100 : $percentage_fail_success;
+				?>
+			<div class="next-card action_btn">
+				<div class="next-card-content df fdr jcsb aic">
+					<div class="df fdc fg-5">
+						<h3><?= $crime[$i] ?></h3>
+						<span><?= $crime_chance[$i] ?>% sannsynelighet</span>
+						<span><?= $crime_success[$i] ?> av <?= $crime_fails[$i] ?> vellykkede forsøk (<?= round($percentage_fail_success_str, 0) ?>%)</span>
+					</div>
+					<div class="df fdc fg-5">
+						<span>Bilverdi <?= $crime_money_from[$i] ?> til <?= number($crime_money_to[$i]) ?> kr</span>
+						<span>Rankpoeng <?= $crime_exp[$i] ?></span>
+						<span>Ventetid <?= $crime_cooldown[$i] ?> sekunder</span>
+					</div>
+				</div>
+			</div>
+			<?php } ?>
+		</div>
+		<div class="innerDiv">
+			<div class="content_header df jcsb">
+				<span class="df aic fg-5">
+					<iconify-icon class="iconify-for-header" icon="mdi:clock-time-five-outline"></iconify-icon> 
+					Aktive konkurranser
+				</span>
+				<a href="#" class="secondary-link df aic">
+					<iconify-icon class="iconify-for-header" icon="material-symbols:double-arrow"></iconify-icon>
+					Gå til konkurranse senter
+				</a>
+			</div>
+			<div class="content_context_narrow-2 df fdr jcsb">
+				<div class="innerDiv w-100 mr-5">
+					<div class="tac content_context df fdc fg-10">
+						<h3>Timeskonkurranse</h3>
+						<h2 class="text-green">00:23:52</h2>
+						<div class="df fdc">
+							<span>Din plassering denne timen er <span class="text-green">#3</span></span>
+							<span>med 23 kriminaliteter og 12 biltyveri</span>
+						</div>
+					</div>
+				</div>
+				<div class="innerDiv w-100 ml-5">
+					<div class="tac content_context df fdc fg-10">
+						<h3>Daglig konkurranse</h3>
+						<h2 class="text-green">08:02:56</h2>
+						<div class="df fdc">
+							<span>Din plassering i dag er <span class="text-green">#4</span></span>
+							<span>med 78 kriminaliteter og 34 biltyveri</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
 
+<script src="js/next-js-hover.js"></script>
 <script type="text/javascript">
 
 	$(".table").on(
