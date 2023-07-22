0948c8821e5e2149f291ff6a77829e0b192f5b9f
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2023-07-22 15:38:27 +0200

    💄 limit list to 10 and get rankls

--- homepage/components/left_menu.php
+++ homepage/components/left_menu.php
@@ -1,3 +1,9 @@
+	<?php
+
+	require_once '../functions/ranks.php';
+
+	?>
+	
 	<div class="left_menu">
 		<div class="aic df jcsb shadow left_menu_element">
 			<span>Topplisten siste timen</span>
@@ -8,14 +14,18 @@
 	 * TODO: Order by EXP last hour
 	 * COMMENT: Currently no order because there is no table with EXP
 	 */
-	$allPlayers = $db->run("SELECT * FROM account")->fetchAll();
+	$allPlayers = $db->run("SELECT a.ACC_name, uv.UV_rank
+	FROM account a
+	JOIN user_values uv ON a.ACC_id = uv.UV_acc_id
+	ORDER BY CAST(uv.UV_EXP AS SIGNED) DESC
+	LIMIT 10")->fetchAll();
 	$i = 1;
 	foreach($allPlayers as $players){
 
 	?>
 		<div class="left_menu_element shadow df aic fg-5">
 			<span class="menu_cooldown"><?= $i ?></span>
-			<span><?= $players['ACC_name'] ?><span class="text-secondary"> • Null</span></span>
+			<span><?= $players['ACC_name'] ?><span class="text-secondary"> • <?= $rankListArray[$players['UV_rank']] ?></span></span>
 		</div>
 	<?php $i++; } ?>
 
@@ -31,14 +41,18 @@
 	 * TODO: Order by total EXP
 	 * COMMENT: Currently no order because there is no table with EXP
 	 */
-	$allPlayers = $db->run("SELECT * FROM account")->fetchAll();
+	$allPlayers = $db->run("SELECT a.ACC_name, uv.UV_rank
+	FROM account a
+	JOIN user_values uv ON a.ACC_id = uv.UV_acc_id
+	ORDER BY CAST(uv.UV_EXP AS SIGNED) DESC
+	LIMIT 10")->fetchAll();
 	$i = 1;
 	foreach($allPlayers as $players){
 
 	?>
 		<div class="left_menu_element shadow df aic fg-5">
 			<span class="menu_cooldown"><?= $i ?></span>
-			<span><?= $players['ACC_name'] ?><span class="text-secondary"> • Null</span></span>
+			<span><?= $players['ACC_name'] ?><span class="text-secondary"> • <?= $rankListArray[$players['UV_rank']] ?></span></span>
 		</div>
 	<?php $i++; } ?>
 </div>
